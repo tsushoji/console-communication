b2ef9dd898c7ddcb4d7739430f0d48ddc6352e3b
tsusho <s.tuji3007sho@gmail.com>
2021-08-09 04:35:01 +0900

    インデントをタブから半角スペース2つ分に修正 #1

--- src/main/java/communication/Main.java
+++ src/main/java/communication/Main.java
@@ -6,22 +6,22 @@ import communication.udp.UDPCom;
 
 public class Main {
 
-	public static void main(String[] args) {
-		UDPComInputStr("localhost", 10005, 1024);
-	}
+  public static void main(String[] args) {
+    UDPComInputStr("localhost", 10005, 1024);
+  }
 
-	private static void UDPComInputStr(String IPAddres, int port, int receivedDataSize) {
-		UDPCom com = new UDPCom();
-		com.coonect(port, receivedDataSize);
-		int inputStrCount = 1;
-		try(Scanner scan = new Scanner(System.in);){
-			while(scan.hasNext()) {
-				String inputStr = scan.nextLine();
-				System.out.println(inputStrCount++ + "-コンソール送信文字列:" + inputStr);
-				com.send(IPAddres, port, inputStr.getBytes("UTF-8"));
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+  private static void UDPComInputStr(String IPAddres, int port, int receivedDataSize) {
+    UDPCom com = new UDPCom();
+    com.coonect(port, receivedDataSize);
+    int inputStrCount = 1;
+    try (Scanner scan = new Scanner(System.in);) {
+      while (scan.hasNext()) {
+        String inputStr = scan.nextLine();
+        System.out.println(inputStrCount++ + "-コンソール送信文字列:" + inputStr);
+        com.send(IPAddres, port, inputStr.getBytes("UTF-8"));
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
--- src/main/java/communication/udp/UDPCom.java
+++ src/main/java/communication/udp/UDPCom.java
@@ -8,45 +8,47 @@ import java.util.Arrays;
 
 public class UDPCom {
 
-	private static int receiveCount = 1;
-	private static int sendCount = 1;
+  private static int receiveCount = 1;
+  private static int sendCount = 1;
 
-	public void coonect(int port, int dataSize) {
-		System.out.println("非同期受信タスク開始");
-		new Thread(new Runnable() {
-           public void run() {
-        	   startReceiveTask(port, dataSize);
-		    }
-		}).start();
-	}
+  public void coonect(int port, int dataSize) {
+    System.out.println("非同期受信タスク開始");
+    new Thread(new Runnable() {
+      public void run() {
+        startReceiveTask(port, dataSize);
+      }
+    }).start();
+  }
 
-	private void startReceiveTask(int port, int dataSize) {
-		while(true) {
-			receive(port, dataSize);
-		}
-	}
-	private boolean receive(int port, int dataSize) {
-		try(DatagramSocket socket = new DatagramSocket(port)){
-			byte[] receivedData = new byte[dataSize];
-			DatagramPacket packet = new DatagramPacket(receivedData, receivedData.length);
-			socket.receive(packet);
-			System.out.println(receiveCount++ + "-UDP通信受信文字列:" + new String(Arrays.copyOf(packet.getData(), packet.getLength()),"UTF-8"));
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
+  private void startReceiveTask(int port, int dataSize) {
+    while (true) {
+      receive(port, dataSize);
+    }
+  }
 
-	public boolean send(String IPAddres, int port, byte[] sendData) {
-		try(DatagramSocket socket = new DatagramSocket()){
-			DatagramPacket packet = new DatagramPacket(sendData, sendData.length, new InetSocketAddress(IPAddres, port));
-			socket.send(packet);
-			System.out.println(sendCount++ + "-UDP通信送信文字列:" + sendData);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
+  private boolean receive(int port, int dataSize) {
+    try (DatagramSocket socket = new DatagramSocket(port)) {
+      byte[] receivedData = new byte[dataSize];
+      DatagramPacket packet = new DatagramPacket(receivedData, receivedData.length);
+      socket.receive(packet);
+      System.out.println(
+          receiveCount++ + "-UDP通信受信文字列:" + new String(Arrays.copyOf(packet.getData(), packet.getLength()), "UTF-8"));
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
+
+  public boolean send(String IPAddres, int port, byte[] sendData) {
+    try (DatagramSocket socket = new DatagramSocket()) {
+      DatagramPacket packet = new DatagramPacket(sendData, sendData.length, new InetSocketAddress(IPAddres, port));
+      socket.send(packet);
+      System.out.println(sendCount++ + "-UDP通信送信文字列:" + sendData);
+    } catch (IOException e) {
+      e.printStackTrace();
+      return false;
+    }
+    return true;
+  }
 }
